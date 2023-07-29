9436738912ba2769c8da5021c5d94db447e39047
Frederic Rieg <fredericrieg@protonmail.com>
2023-07-29 18:29:24 +0300

    created POST request & linked to DB  for generalmessage

--- first_django_app/chat/views.py
+++ first_django_app/chat/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Message, Chat
 
 # Create your views here.
 def index(request):
     if request.method == 'POST':
         print("Received data: " + request.POST['generalmessage'])
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(text=request.POST['generalmessage'], chat=myChat, author=request.user, receiver=request.user)
     return render(request, 'chat/index.html', {'username': "Frederic"})
