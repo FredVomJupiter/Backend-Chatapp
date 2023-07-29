8bc9eec50ed64509ef1b8506550044d787a1e2e6
Frederic Rieg <fredericrieg@protonmail.com>
2023-07-29 18:28:50 +0300

    added Chat to admin panel

--- first_django_app/chat/admin.py
+++ first_django_app/chat/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
-from .models import Message
+from .models import Chat, Message
 
 
 class MessageAdmin(admin.ModelAdmin):
-    fields=('text', 'created_at', 'author', 'receiver')
+    fields=('chat', 'text', 'created_at', 'author', 'receiver')
     list_display=('text', 'created_at', 'author', 'receiver')
     search_fields=('text',)
+
+
+class ChatAdmin(admin.ModelAdmin):
+    fields=('created_at',)
+    list_display=('created_at',)
+    search_fields=('created_at',)
 
 # Register your models here.
 
 admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat, ChatAdmin)
