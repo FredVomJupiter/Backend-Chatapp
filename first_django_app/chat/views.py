4eae104de23e9c29608d5896681f7b3344bc7bc4
Frederic Rieg <fredericrieg@protonmail.com>
2023-07-29 17:11:37 +0300

    implemented post req for Chat with csrf_token

--- first_django_app/chat/views.py
+++ first_django_app/chat/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        print("Received data: " + request.POST['generalmessage'])
     return render(request, 'chat/index.html', {'username': "Frederic"})
