8f187f65a049a49ab89f8bb8a424bd1ebf5db7f0
Frederic Rieg <fredericrieg@protonmail.com>
2023-08-22 20:08:37 +0300

    post messages & view them in channel

--- first_django_app/chat/views.py
+++ first_django_app/chat/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Message, Chat
+from django.template import RequestContext
 
 # Create your views here.
 def index(request):
@@ -7,4 +8,5 @@ def index(request):
         print("Received data: " + request.POST['generalmessage'])
         myChat = Chat.objects.get(id=1)
         Message.objects.create(text=request.POST['generalmessage'], chat=myChat, author=request.user, receiver=request.user)
-    return render(request, 'chat/index.html', {'username': "Frederic"})
+    chatMessages = Message.objects.filter(chat__id=1)
+    return render(request, 'chat/index.html', {'messages': chatMessages})
