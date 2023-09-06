6b692a054816abd52dcbcca26c91ae9a73dcf635
Frederic Rieg <fredericrieg@protonmail.com>
2023-09-06 15:17:55 +0300

    implemented django user registration

--- first_django_app/chat/views.py
+++ first_django_app/chat/views.py
@@ -1,20 +1,27 @@
-from django.http import HttpResponse, HttpResponseRedirect
-from django.template import loader
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+
 from .models import Message, Chat
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import NewUserForm
 
 # Create your views here.
+@login_required(login_url='/login/')
 def index(request):
     if request.method == 'POST':
         print("Received data: " + request.POST['generalmessage'])
-        myChat = Chat.objects.get(id=1)
-        Message.objects.create(text=request.POST['generalmessage'], chat=myChat, author=request.user, receiver=request.user)
+        if Chat.objects.filter(id=1).count() > 0:
+            myChat = Chat.objects.get(id=1)
+            Message.objects.create(text=request.POST['generalmessage'], chat=myChat, author=request.user, receiver=request.user)
+        else: 
+            myChat = Chat.objects.create(id=1)
+            Message.objects.create(text=request.POST['generalmessage'], chat=myChat, author=request.user, receiver=request.user)
         chatMessages = Message.objects.filter(chat__id=1)
         return render(request, 'chat/index.html', {'messages': chatMessages})
-        logout(request)
-        return HttpResponseRedirect('/logout/')
-    return render(request, 'chat/index.html')
+    chatMessages = Message.objects.filter(chat__id=1)
+    return render(request, 'chat/index.html', {'messages': chatMessages})
 
 
 def login_view(request):
@@ -27,11 +34,6 @@ def login_view(request):
             print("User logged in")
             return HttpResponseRedirect('/chat/')
     return render(request, 'login/login.html')
-        #else:
-         #   print("User not logged in")
-          #  return render(request, 'login/login.html')
-    #else:
-     #   return render(request, 'login/login.html')
 
 
 def logout_view(request):
@@ -41,14 +43,14 @@ def logout_view(request):
 
 def register_view(request):
     if request.method == "POST":
-        email = request.POST.get('email')
-        password = request.POST.get('firstPW')
-        repeatPassword = request.POST.get('secondPW')
-        if password == repeatPassword:
-            print("Passwords match")
-            print("User registered")
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+            messages.success(request, "Registration successful." )
             return HttpResponseRedirect('/login/')
         else:
-            print("Passwords do not match")
+            messages.error(request, "Unsuccessful registration. Invalid information.")
             return render(request, 'register/register.html')
+    form = NewUserForm()
     return render(request, 'register/register.html')
