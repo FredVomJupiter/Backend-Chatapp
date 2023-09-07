e81ccfbd69bdb6b0546ce6590c157d217b8dd9bf
Frederic Rieg <fredericrieg@protonmail.com>
2023-09-07 10:28:46 +0300

    improved redirect to login

--- first_django_app/first_django_app/urls.py
+++ first_django_app/first_django_app/urls.py
@@ -21,9 +21,10 @@ from chat.views import index
 from chat.views import login_view
 from chat.views import logout_view
 from chat.views import register_view
+from chat.views import redirect_view
 
 urlpatterns = [
-    path('', login_view, name='login'),
+    path('', redirect_view, name='redirect'),
     path('admin/', admin.site.urls),
     path('chat/', index),
     path('login/', login_view, name='login'),
